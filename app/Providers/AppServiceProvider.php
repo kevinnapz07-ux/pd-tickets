a1be55051f3a5d3df905a1eed01cf0875b76059c
Kevin Napitupulu <kevinnapz07@gmail.com>
2026-07-17 12:47:21 +0700

    Fix HTTPS handling on Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,7 @@ use Illuminate\Auth\Events\Logout;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Throwable;
 
@@ -31,6 +32,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+        }
+        
         $this->warnAboutProductionMailConfiguration();
 
         Event::listen(Login::class, function (Login $event): void {
