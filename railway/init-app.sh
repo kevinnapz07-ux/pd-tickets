9864ec9c1b72971a46e932485cbcfc1dec62baf5
Kevin Napitupulu <kevinnapz07@gmail.com>
2026-07-19 02:33:21 +0700

    Fix public theme colors and uploaded media access

--- railway/init-app.sh
+++ railway/init-app.sh
@@ -4,6 +4,7 @@ set -eu
 php artisan optimize:clear
 php artisan migrate --force
 php artisan db:seed --class=AdminUserSeeder --force
+php artisan storage:link --force
 php artisan config:cache
 php artisan event:cache
 php artisan view:cache
